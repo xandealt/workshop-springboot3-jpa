2aea95631730dcf6ffd40b8cd04d71a673408330
Alexandre Augusto <alexandre_alt@outlook.com>
2023-06-14 13:30:56 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/springcourse/course/entities/User.java
+++ src/main/java/com/springcourse/course/entities/User.java
@@ -1,11 +1,16 @@
 package com.springcourse.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,10 @@ public class User implements Serializable {
   private String email;
   private String phone;
   private String password;
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "client")
+  private List<Order> orders = new ArrayList<>();
 
   public User() {
 
@@ -72,6 +81,10 @@ public class User implements Serializable {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  public List<Order> getOrders() {
+    return orders;
   }
 
   @Override
