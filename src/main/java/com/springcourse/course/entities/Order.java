c91f87a19beb6d217fd1aa4522de40a6d1fb3d87
Alexandre Tavares <alexandre_alt@outlook.com>
2023-06-19 13:38:07 -0300

    Subtotal & Total methods

--- src/main/java/com/springcourse/course/entities/Order.java
+++ src/main/java/com/springcourse/course/entities/Order.java
@@ -100,6 +100,14 @@ public class Order implements Serializable {
     this.payment = payment;
   }
 
+  public Double getTotal() {
+    Double total = 0.0;
+    for (OrderItem item : items) {
+      total += item.getSubTotal();
+    }
+    return total;
+  }
+
   @Override
   public int hashCode() {
     final int prime = 31;
